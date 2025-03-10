1ab1f58202ca2a49895e287b97aee4d66413b1f5
Ma YouPeng <mma307405@gmail.com>
2025-03-10 20:26:23 +0800

    first commit!

---
+++ src/test/java/com/itheima/git_demo/GitDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.itheima.git_demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GitDemoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
